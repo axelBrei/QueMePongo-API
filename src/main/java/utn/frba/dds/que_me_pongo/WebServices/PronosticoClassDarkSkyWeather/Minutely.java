58857a7a46204882bf73bae3cd0472d24230583f
axelbrei <axelbreiterman@gmail.com>
2019-08-02 20:00:53 -0300

    QMP-6: cambio olvidado de la parte de mover archivos relacionados a las api de clima de lugar

---
+++ src/main/java/utn/frba/dds/que_me_pongo/WebServices/PronosticoClassDarkSkyWeather/Minutely.java
@@ -0,0 +1,30 @@
+package utn.frba.dds.que_me_pongo.WebServices.PronosticoClassDarkSkyWeather;
+
+import java.util.ArrayList;
+
+public class Minutely {
+    private String summary;
+    private String icon;
+    ArrayList< Object > data = new ArrayList < Object > ();
+
+
+    // Getter Methods
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    // Setter Methods
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+}
