56ceba93f2f599e003c92081eac1daf5d8a0162c
Kevin <kevin_nw@hotmail.com>
2019-11-07 18:54:48 -0300

    Subir una foto y descargarla

--- src/main/java/utn/frba/dds/que_me_pongo/Controller/PrendasController.java
+++ src/main/java/utn/frba/dds/que_me_pongo/Controller/PrendasController.java
@@ -1,15 +1,18 @@
 package utn.frba.dds.que_me_pongo.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import utn.frba.dds.que_me_pongo.Model.Cliente;
 import utn.frba.dds.que_me_pongo.Model.Guardarropa;
 import utn.frba.dds.que_me_pongo.Model.Prenda;
@@ -21,13 +24,26 @@ import utn.frba.dds.que_me_pongo.Utilities.WebServices.Request.Prenda.DeletePren
 import utn.frba.dds.que_me_pongo.Utilities.WebServices.Request.Prenda.NuevaPrendaRequest;
 import utn.frba.dds.que_me_pongo.Utilities.WebServices.Responses.GetPrendasResponse;
 
+import javax.activation.FileTypeMap;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.Set;
+
+import static org.apache.tomcat.util.http.fileupload.IOUtils.copy;
 
 @RestController
 @RequestMapping("/prendas")
 public class PrendasController {
+    String fileBasePath = "src/main/resources/fotos/";
     @Autowired
     ClientesRepository clientesRepository;
     @Autowired
@@ -69,5 +85,46 @@ public class PrendasController {
 
         return new ResponseEntity<>("Prenda eliminada con exito", HttpStatus.OK);
     }
+
+    @PostMapping(value = "uploadFoto")
+    public ResponseEntity uploadToLocalFileSystem(@RequestParam("file") MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        Path path = Paths.get(fileBasePath + fileName);
+        try {
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/prendas/fotos/")
+                .path(fileName)
+                .toUriString();
+
+        return ResponseEntity.ok(fileDownloadUri);
+    }
+
+    @RequestMapping(value = "/fotos/{file_name}", method = RequestMethod.GET)
+    public ResponseEntity<Resource> download( @PathVariable("file_name") String fileName) throws IOException {
+        HttpHeaders header = new HttpHeaders();
+        header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=img.jpg");
+        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        header.add("Pragma", "no-cache");
+        header.add("Expires", "0");
+
+        Path path = Paths.get(fileBasePath + fileName);
+        File file = new File(path.toUri());
+
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        HttpHeaders headers;
+        return ResponseEntity.ok()
+                .headers(header)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
+    }
+
+
+
 
 }
