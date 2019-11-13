b3a7d124529afaf4199c62dd60e929e00d6d2308
Kevin <kevin_nw@hotmail.com>
2019-11-12 21:45:27 -0300

    Nueva query de prendas ocupadas

--- src/main/java/utn/frba/dds/que_me_pongo/Helpers/ReservaHelper.java
+++ src/main/java/utn/frba/dds/que_me_pongo/Helpers/ReservaHelper.java
@@ -7,15 +7,29 @@ import utn.frba.dds.que_me_pongo.Model.Atuendo;
 import utn.frba.dds.que_me_pongo.Model.Evento;
 import utn.frba.dds.que_me_pongo.Model.Prenda;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
+import utn.frba.dds.que_me_pongo.Helpers.PrendasReservadas;
 
 @NoArgsConstructor
 public class ReservaHelper {
     public  List<Prenda> prendasDisponibles(List<Prenda> prendas, Evento e, List<PrendasReservadas> reservadas){
         List<Prenda> filtradas =   prendas.stream().filter( p -> !estaReservada(p,e.getDesde(),e.getHasta(),reservadas) ).collect(Collectors.toList());
         return  filtradas;
+    }
+
+    public List<Prenda> getLibres(List<Prenda> prendas , List<PrendasReservadas> ocupadas){
+        List<Prenda> PrendasLibres = new ArrayList<>();
+
+        prendas.forEach(p ->{
+            if(ocupadas.stream().noneMatch(l->l.getPrendas_id().equals(p.getId().longValue())))
+                PrendasLibres.add(p);
+        });
+
+        return  PrendasLibres;
+
     }
 
     public  Boolean sePuedeReservarAtuendo(Atuendo a, Evento e, List<PrendasReservadas> reservadas){
--- src/main/java/utn/frba/dds/que_me_pongo/Repository/PrendaReservadaRespository.java
+++ src/main/java/utn/frba/dds/que_me_pongo/Repository/PrendaReservadaRespository.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.Transactional;
 import utn.frba.dds.que_me_pongo.Helpers.PrendasReservadas;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PrendaReservadaRespository {
 
     List<PrendasReservadas> prendasReservadasList();
+    List<PrendasReservadas> prendasOcupadas(Date desde, Date hasta, Integer guardarropa );
 }
